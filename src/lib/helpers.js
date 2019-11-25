70accf169964c16dee482d7ca61406eb1ffa5fc0
fas19 <47185536+fas19@users.noreply.github.com>
2019-11-25 15:08:34 +0000

    sameina kóða

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -3,3 +3,75 @@ export function empty(element) {
     element.removeChild(element.firstChild);
   }
 }
+function displayLecture(el, data){
+  let thumbnail;
+  if(data.thumbnail == null){
+    thumbnail = 'engin mynd';
+  }else{
+    thumbnail = data.thumbnail;
+  }
+  const category = data.category;
+  const title = data.title;
+
+  const lecture = document.createElement('div');
+  lecture.className ='lecture';
+  const lectureTitle = document.createElement('div');
+  lectureTitle.className = 'lecture__title';
+  const lectureCategory = document.createElement('div');
+  lectureCategory.className = 'lecture__category';
+  const lectureImage = document.createElement('div');
+  lectureImage.className = 'lecture__image';
+  const lectureTitleH2 = document.createElement('h2');
+  lectureTitleH2.className = 'headline2';
+  const lectureCategoryH3 = document.createElement('h3');
+  lectureCategoryH3.className = 'headline3';
+
+  // Hér þarf að koma virkni til að virkja ef búið er að klára að lesa fyrirlesturinn..
+lectureTitleH2.appendChild(document.createTextNode(title));
+
+lectureCategoryH3.appendChild(document.createTextNode(category));
+
+const lectureImageImg = document.createElement('img');
+lectureImageImg.className = 'img';
+if(thumbnail == 'engin mynd'){
+  lectureImageImg.alt = 'engin mynd';
+}else{
+  lectureImageImg.src = thumbnail;
+
+}
+// setjum saman
+
+lectureImage.appendChild(lectureImageImg);
+lectureCategory.appendChild(lectureCategoryH3);
+lectureTitle.appendChild(lectureTitleH2);
+lecture.appendChild(lectureImage);
+lecture.appendChild(lectureTitle);
+lecture.appendChild(lectureCategory);
+const grid = document.querySelector('.grid');
+el.appendChild(lecture);
+
+
+
+}
+
+function allEqual(bool) {
+  if (bool[0] === bool[1] && bool[1] === bool[2]) {
+    return true;
+  }
+  return false;
+}
+export function displayLectures(el, lectKeys, lectures, buttonBool){
+  const element = el;
+  const lectureKeys = lectKeys;
+  const allLectures = lectures;
+  for (var x in allLectures){
+    console.log((allLectures[x]));
+    //vil setja inn æi fallið displayLecture allt data ef bool er okey.
+    if(allEqual(buttonBool) || buttonBool[1] && allLectures[x].category == 'html'){
+      //console.log(allLectures[x]);
+      displayLecture(el, allLectures[x]);
+    }
+
+
+  }
+}
--- src/lib/list.js
+++ src/lib/list.js
@@ -1,11 +1,75 @@
-import { empty } from './helpers';
+import { empty, displayLectures } from './helpers';
+
+
+const buttonBoolean = new Array(3).fill(false);
+const lecKeys = ['title', 'catagory', 'thumbnamil'];
+const DATA_URL = '/lectures.json';
+let jsonData;
+
+function onClickHTML(e){
+  console.log('yo');
+ }
+ function onClickCss(e){
+   console.log('yo'); 
+ }
+ function onClickJs(e){
+ console.log('yo');
+ }
+ function loadLecture(e){
+   //debugger;
+   console.log(e.target);
+   const parent = e.target.parentNode.parentNode;
+   const blah = parent.querySelector('.headline2').textContent;
+   //var hehe = blah.textContent;
+   for(var x of jsonData.lectures){
+     if (x.title == blah){
+       //console.log('scooore');
+       var slug = x.slug;
+       console.log('viljum fara á ' + slug);
+       sessionStorage.setItem('data', JSON.stringify(x));
+       window.location.href = (`http://localhost:3000/fyrirlestur.html?slug=${slug}`);
+       //window.location.href = (`http://localhost:3000/fyrirlestur.html`);
+     }
+   }
+ }
+ function addEventHandlers(){
+   const lectures = document.getElementsByClassName('lecture');
+   for(var lecture of lectures){
+     lecture.addEventListener('click', loadLecture);
+     console.log('added listener');
+   }
+   
+ }
 
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
+    this.htmlButt = document.querySelector('.html-butt');
+    this.cssButt = document.querySelector('.css-butt');
+    this.jsButt = document.querySelector('.js-butt');
   }
+
 
   load() {
     empty(this.container);
+    this.htmlButt.addEventListener('click', onClickHTML);
+    this.cssButt.addEventListener('click', onClickCss);
+    this.jsButt.addEventListener('click', onClickJs);
+    
+    fetch(DATA_URL)
+    .then((response) => {
+      if(response.ok){
+        return response.json();
+      }
+      throw new Error('Villa við að sækja gögn');
+    })
+    .then((data) => {
+      jsonData = data;
+      displayLectures(this.container, lecKeys, data.lectures, buttonBoolean);
+      addEventHandlers();
+    })
+    .catch((error) => {
+      console.log(error);  //  eslint-disable-line
+    });
   }
 }

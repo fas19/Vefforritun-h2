ff7be2ce45bc8eaa0390b78ce94430e8a60fb1da
fas19 <47185536+fas19@users.noreply.github.com>
2019-11-25 15:10:12 +0000

    update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const isLecturePage = page.classList.contains('lecture-page');
 
   if (isLecturePage) {
+    debugger;
+    console.log('lecture load');
 
   } else {
     const list = new List();
